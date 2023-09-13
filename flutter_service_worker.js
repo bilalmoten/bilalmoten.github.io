ff771958007098aaaa821a6e7c56c83708df4086
bilalmoten <82532206+bilalmoten@users.noreply.github.com>
2023-09-13 15:07:23 +0500

    buy more button
    button added on the Home Screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "45baf888175553a78a2f8bf8105a684d",
 "favicon.ico": "db2dd6d7682e5505b8a0c157daf84f3f",
-"index.html": "2ac29c78309f92b9c137a1ef97aefb09",
-"/": "2ac29c78309f92b9c137a1ef97aefb09",
+"index.html": "946ec3c3197eaf1bce804a2fb561a3bb",
+"/": "946ec3c3197eaf1bce804a2fb561a3bb",
 "main.dart.js": "0b1ed6a379cd3695dfd8951a5b67e364",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "0bcf7d7f007544d20f66fd82990e6d4f",
